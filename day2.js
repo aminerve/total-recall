1efd100cde84419dbde1eb1591be8e0b55bd3066
aminerve <122647915+aminerve@users.noreply.github.com>
2023-02-16 12:05:24 -0500

    Wild Wild Life answered

--- day2.js
+++ day2.js
@@ -17,23 +17,35 @@
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-for (let i=1;i<=100;i++){
-    if (i%3===0 && i%5===0){
-        console.log('FizzBuzz');
-    } else if(i%3===0){
-        console.log('Fizz');
-    } else if(i%5===0){
-        console.log('Buzz');
-    }else{
-        console.log(i);
-    }
-}
+// for (let i=1;i<=100;i++){
+//     if (i%3===0 && i%5===0){
+//         console.log('FizzBuzz');
+//     } else if(i%3===0){
+//         console.log('Fizz');
+//     } else if(i%5===0){
+//         console.log('Buzz');
+//     }else{
+//         console.log(i);
+//     }
+// }
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-
+plantee[2] = 187
+wolfy[3] = 'Gotham City'
+dart.push('Hawkins')
+wolfy.shift()
+wolfy.unshift('Gameboy')
+console.log(plantee);
+console.log(wolfy);
+console.log(dart);
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
